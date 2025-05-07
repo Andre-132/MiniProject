9e5da22551e7392a6f2fba9edf4b2e012199eb7d
Andre Albarado <analbarado1@gmail.com>
2025-05-07 15:32:00 -0500

    changes

--- landpage/LandingPage.js
+++ landpage/LandingPage.js
@@ -1,3 +1,5 @@
-function startGame() {
-  alert("Game Starting...");
+function goToGamePage() {
+  window.location.href = "../game/game.html";
 }
+
+document.getElementById("play-game").addEventListener("click", goToGamePage);
