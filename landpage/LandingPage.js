7f7f8e3409ebe40062e2ccb0e0382e2008b41a36
Andre Albarado <analbarado1@gmail.com>
2025-05-09 22:15:42 -0500

    changes

--- landpage/LandingPage.js
+++
@@ -1,5 +0,0 @@
-function goToGamePage() {
-  window.location.href = "../game/game.html";
-}
-
-document.getElementById("play-game").addEventListener("click", goToGamePage);
--- main/landpage/LandingPage.js
+++ main/landpage/LandingPage.js
@@ -1,18 +1,3 @@
-function getFact() {
-  fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
-    .then((response) => response.JSON())
-    .then((data) => {
-      document.getElementById("fact").textContent = data.text;
-    })
-
-    .catch((error) => {
-      console.error("Error fetching fact:", error);
-      document.getElementById("fact").textContent =
-        "Oops, something went wrong!";
-    });
-}
-getFact();
-
 function goToGamePage() {
   window.location.href = "../game/game.html";
 }
