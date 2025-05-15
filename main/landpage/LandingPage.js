a8c6fa08975d15e605436454d7ea7744ebbd0ebc
Andre Albarado <analbarado1@gmail.com>
2025-05-14 20:16:32 -0500

    changes

--- main/landpage/LandingPage.js
+++ main/landpage/LandingPage.js
@@ -3,3 +3,20 @@ function goToGamePage() {
 }
 
 document.getElementById("play-game").addEventListener("click", goToGamePage);
+
+async function getRandomFact() {
+  try {
+    const response = await fetch(
+      "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+    );
+    const data = await response.json();
+    document.getElementById("fact").innerText = data.text;
+  } catch (error) {
+    document.getElementById("fact").innerText = "Failed to load a fact.";
+    console.error("Error:", error);
+  }
+}
+
+document
+  .getElementById("get-fact-button")
+  .addEventListener("click", getRandomFact);
