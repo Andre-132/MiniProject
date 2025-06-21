172dc44a139641599cfcd9edd244fd77e09b3471
Andre Albarado <analbarado1@gmail.com>
2025-06-21 15:28:57 -0500

    Shadcn added

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Home from "./HomePage";
+import { ThemeProvider } from "./components/ui/Themes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Home />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
